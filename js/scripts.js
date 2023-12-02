c7e84bf525b8afd6111aa17a99feacea6e9795c7
sf927 <sf927@drexel.edu>
2023-12-02 16:03:28 -0500

    week-7-update

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,107 @@
-//alert("Hello Class")
+/* --------------------
+    Navigation 
+ -------------------- */  
 
-var mainNav = document.querySelector("#mainNav");
+ var mainNav = document.querySelector("#main-nav");
+ var signInUpButtons = document.querySelector("#sign-in-up-buttons");
+ var burgerBtn = document.querySelector("#burger-container button");
+ 
+ var burgerBars = document.querySelector("#burger-bars");
+ var burgerX = document.querySelector("#burger-x");
+ 
+ 
+ // this can only be true or false known as a boolean
+ var canYouSeeMainNav = false;
+ 
+ function showNav(){
+     // console.log("button clicked");
+     if(canYouSeeMainNav === false){
+         console.log("show menu");
+         // this will add the class of showMainNav to the id mainNav
+         mainNav.classList.add("showMainNav");
+ 
+         signInUpButtons.classList.add("showSignIn");
+         canYouSeeMainNav = true;
+ 
+         // hide burger bars
+         burgerBars.classList.add("hide-burger-bars");
+ 
+         // show burger x
+         burgerX.classList.remove("hide-burger-x");
+ 
+     }else{
+         console.log("hide menu");
+         // this will remove the class of showMainNav to the id mainNav
+         mainNav.classList.remove("showMainNav");
+ 
+         signInUpButtons.classList.remove("showSignIn");
+ 
+         // show burger bars
+         burgerBars.classList.remove("hide-burger-bars");
+ 
+         // hide burger x
+         burgerX.classList.add("hide-burger-x");
+ 
+         canYouSeeMainNav = false;
+     }
+     
+ }
+ 
+ // add a event listener to the button
+ burgerBtn.addEventListener("click", showNav);
+ 
+ 
+ 
+ /* --------------------
+     Sign In 
+  -------------------- */  
+  var signInBtn = document.querySelector("#sign-in-btn");
+  var signInContainer = document.querySelector("#sign-in-container");
+  var closeSignUpModalBtn = document.querySelector("#close-sign-up-modal-btn");
+ 
+ 
+  function showSignInModal() {
+     // console.log("show modal now");
+     signInContainer.classList.add("show-signIn-container");
+     // hide the navigation on close
+     mainNav.classList.remove("showMainNav");
+     // hide the signup/in buttons
+     signInUpButtons.classList.remove("showSignIn");
+  }
+ 
+  function hideSignInModal(){
+     signInContainer.classList.remove("show-signIn-container");
+     
+  }
+ 
+  signInBtn.addEventListener("click", showSignInModal);
+  closeSignUpModalBtn.addEventListener("click", hideSignInModal);
+ 
+  signInContainer.addEventListener("click", hideSignInModal);
 
-var sign = document.querySelector("#sign");
-
-var burgerBtn = document.querySelector("#berger-container button")
-
-var canYouSeeMainNav = false;
-
-//var sectionTags= document.querySelectorAll("section");
 
 
+  // Add the following JavaScript code to your existing scripts-dist.js file
 
-function showNav(){
-    //console.log("button clicked");
-    if(canYouSeeMainNav === false){
-        console.log("show menu");
-        mainNav.classList.add("showMainNav");
+var signUpBtn = document.querySelector(".sign-out");
+var signUpModalContainer = document.querySelector("#signUpModal-container");
+var closeSignUpModalBtn = document.querySelector("#close-signUp-modal-btn");
+var cancelSignUpModalBtn = document.querySelector("#cancel-signUp-modal-btn");
 
-        sign.classList.add("showSign");
-        canYouSeeMainNav = true;
-
-    }else{
-        console.log("hide menu");
-        mainNav.classList.remove("showMainNav");
-        
-        sign.classList.remove("showSign");
-        canYouSeeMainNav = false;
-        
-    }
-    
+function showSignUpModal() {
+    signUpModalContainer.classList.add("showsignUpModalcontainer");
+    mainNav.classList.add("showMainNav");
 }
 
-burgerBtn.addEventListener("click", showNav);
+function hideSignUpModal() {
+    signUpModalContainer.classList.remove("showsignUpModalcontainer");
+    mainNav.classList.remove("showMainNav");
+}
 
-//showNav();
+signUpBtn.addEventListener("click", showSignUpModal);
+closeSignUpModalBtn.addEventListener("click", hideSignUpModal);
+cancelSignUpModalBtn.addEventListener("click", hideSignUpModal);
+signUpModalContainer.addEventListener("click", hideSignUpModal);
+
+  
+ 
